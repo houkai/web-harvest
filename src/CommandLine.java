5c72039acf4de814c1eaf3fc1aa0304ab1f1f505
vnikic <vnikic@451d0739-362e-0410-8668-fe84cbc230cf>
2007-09-02 09:06:09 +0000

    git-svn-id: https://web-harvest.svn.sourceforge.net/svnroot/web-harvest@31 451d0739-362e-0410-8668-fe84cbc230cf

--- src/CommandLine.java
+++ src/CommandLine.java
@@ -39,51 +39,66 @@ import org.apache.log4j.PropertyConfigurator;
 import org.webharvest.definition.ScraperConfiguration;
 import org.webharvest.runtime.Scraper;
 import org.webharvest.gui.Ide;
+import org.webharvest.utils.CommonUtil;
 
 import javax.swing.*;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.net.URL;
+import java.net.MalformedURLException;
 
+/**
+ * Startup class  for Web-Harvest.
+ */
 public class CommandLine {
 	
-	private static String getArgValue(String[] args, String name) {
-		for (int i = 0; i < args.length; i++) {
+	private static Map getArgValue(String[] args) {
+        Map params = new HashMap();
+        for (int i = 0; i < args.length; i++) {
 			String curr = args[i];
+            String argName = curr.toLowerCase();
+            String argValue = "";
+
 			int eqIndex = curr.indexOf('=');
 			if (eqIndex >= 0) {
-				String argName = curr.substring(0, eqIndex).trim(); 
-				String argValue = curr.substring(eqIndex+1).trim();
-				
-				if (argName.toLowerCase().startsWith(name.toLowerCase())) {
-					return argValue;
-				}
-			}
-		}
+				argName = curr.substring(0, eqIndex).trim();
+				argValue = curr.substring(eqIndex+1).trim();
+            }
+
+            params.put(argName.toLowerCase(), argValue);
+        }
 		
-		return ""; 
+		return params; 
 	}
 	
-    public static void main(String[] args) throws FileNotFoundException {
-        if ( args == null || args.length == 0 || !"-cl".equals(args[0]) ) {
+    public static void main(String[] args) throws IOException {
+        Map params = getArgValue(args);
+
+        if (params.size() == 0) {
             SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
                     new Ide().createAndShowGUI();
                 }
             });
+        } else if ( params.containsKey("-h") || params.containsKey("/h")) {
+            printHelp();
+            System.exit(0);
         } else {
-            String configFilePath = getArgValue(args, "config");
-            if ("".equals(configFilePath)) {
+            String configFilePath = (String) params.get("config");
+            if ( configFilePath == null || "".equals(configFilePath) ) {
                 System.err.println("You must specify configuration file path using config=<path> argument!");
+                printHelp();
                 System.exit(1);
             }
 
-            String workingDir = getArgValue(args, "workdir");
-            if ("".equals(workingDir)) {
-                System.err.println("You must specify working directory path using workdir=<path> argument!");
-                System.exit(1);
+            String workingDir = (String) params.get("workdir");
+            if ( workingDir == null || "".equals(workingDir) ) {
+                workingDir = ".";
             }
-
-            String isDebug = getArgValue(args, "debug");
 
             Properties props = new Properties();
             props.setProperty("log4j.rootLogger", "INFO, stdout");
@@ -99,17 +114,25 @@ public class CommandLine {
 
             PropertyConfigurator.configure(props);
 
-            ScraperConfiguration config = new ScraperConfiguration(configFilePath);
+            ScraperConfiguration config = null;
+            String configLowercase = configFilePath.toLowerCase();
+            if ( configLowercase.startsWith("http://") || configLowercase.startsWith("https://") ) {
+                config = new ScraperConfiguration( new URL(configFilePath) );
+            } else {
+                config = new ScraperConfiguration(configFilePath);
+            }
+
             Scraper scraper = new Scraper(config, workingDir);
 
-            if ("yes".equalsIgnoreCase(isDebug)) {
+            String isDebug = (String) params.get("debug");
+            if ( CommonUtil.isBooleanTrue(isDebug) ) {
                 scraper.setDebug(true);
             }
 
-            String proxyHost = getArgValue(args, "proxyHost");
-            if (!"".equals(proxyHost)) {
-                String proxyPort = getArgValue(args, "proxyPort");
-                if (!"".equals(proxyPort)) {
+            String proxyHost = (String) params.get("proxyhost");
+            if ( proxyHost != null && !"".equals(proxyHost)) {
+                String proxyPort = (String) params.get("proxyport");
+                if ( proxyPort != null && !"".equals(proxyPort) ) {
                     int port = Integer.parseInt(proxyPort);
                     scraper.getHttpClientManager().setHttpProxy(proxyHost, port);
                 } else {
@@ -117,14 +140,51 @@ public class CommandLine {
                 }
             }
 
-            String proxyUser = getArgValue(args, "proxyUser");
-            if (!"".equals(proxyUser)) {
-                String proxyPassword = getArgValue(args, "proxyPassword");
+            String proxyUser = (String) params.get("proxyuser");
+            if ( proxyUser != null && !"".equals(proxyUser) ) {
+                String proxyPassword = (String) params.get("proxypassword");
                 scraper.getHttpClientManager().setHttpProxyCredentials(proxyUser, proxyPassword);
+            }
+
+            // adds initial variables to the scraper's content, if any
+            Iterator iterator = params.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry) iterator.next();
+                String key = (String) entry.getKey();
+                if (key.startsWith("#")) {
+                    String varName = key.substring(1);
+                    if (varName.length() > 0) {
+                        scraper.addVariableToContext(varName, entry.getValue());
+                    }
+                }
             }
 
             scraper.execute();
         }
+    }
+
+    private static void printHelp() {
+        System.out.println("");
+        System.out.println("To open Web-Harvest GUI:");
+        System.out.println("   java -jar webharvestXX.jar");
+        System.out.println("or just double-click webharvestXX.jar from the file manager.");
+        System.out.println("");
+        System.out.println("Command line use:");
+        System.out.println("   java -jar webharvestXX.jar [-h] config=<path> [workdir=<path>] [debug=yes|no]");
+        System.out.println("             [proxyhost=<proxy server> [proxyport=<proxy server port>]]");
+        System.out.println("             [proxyuser=<proxy username> [proxypassword=<proxy password>]]");
+        System.out.println("             [#var1=<value1> [#var2=<value2>...]]");
+        System.out.println("");
+        System.out.println("   -h            - shows this help.");
+        System.out.println("   config        - path or URL of configuration (URL must begin with \"http://\" or \"https://\").");
+        System.out.println("   workdir       - path of the working directory (default is current directory).");
+        System.out.println("   debug         - specify if Web-Harvest generates debugging output (default is no).");
+        System.out.println("   proxyhost     - specify proxy server.");
+        System.out.println("   proxyport     - specify port for proxy server.");
+        System.out.println("   proxyuser     - specify proxy server username.");
+        System.out.println("   proxypassword - specify proxy server password.");
+        System.out.println("   #varN, valueN - specify initial variables of the Web-Harvest context. To be recognized, ");
+        System.out.println("                   each variable name must have prefix #. ");
     }
 
 }
