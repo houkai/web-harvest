c34b4bcb5038d7c0bcb4a0ac8f8e2379c4700297
vnikic <vnikic@451d0739-362e-0410-8668-fe84cbc230cf>
2008-08-23 12:06:01 +0000

    database processor development
    git-svn-id: https://web-harvest.svn.sourceforge.net/svnroot/web-harvest@145 451d0739-362e-0410-8668-fe84cbc230cf

--- src/org/webharvest/runtime/processors/plugins/DatabasePlugin.java
+++ src/org/webharvest/runtime/processors/plugins/DatabasePlugin.java
@@ -5,6 +5,11 @@ import org.webharvest.runtime.ScraperContext;
 import org.webharvest.runtime.processors.*;
 import org.webharvest.runtime.variables.NodeVariable;
 import org.webharvest.runtime.variables.Variable;
+import org.webharvest.runtime.variables.ListVariable;
+import org.webharvest.exception.DatabaseException;
+import org.webharvest.utils.CommonUtil;
+
+import java.sql.*;
 
 /**
  * Support for database operations.
@@ -16,16 +21,54 @@ public class DatabasePlugin extends WebHarvestPlugin {
     }
 
     public Variable execute(Scraper scraper, ScraperContext context) {
+        String jdbc = evaluateAttribute("jdbcclass", scraper);
+        String connection = evaluateAttribute("connection", scraper);
+        String username = evaluateAttribute("username", scraper);
+        String password = evaluateAttribute("password", scraper);
+        String rowElement = evaluateAttribute("rowelement", scraper);
+        if ( rowElement == null || "".equals(rowElement.trim()) ) {
+            rowElement = "row";
+        }
+
+        Connection conn = scraper.getConnection(jdbc, connection, username, password);
         Variable body = executeBody(scraper, context);
-        return new NodeVariable(evaluateAttribute("connection", scraper));
+        String sql = body.toString();
+
+        ListVariable queryResult = new ListVariable();
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            statement.execute();
+            ResultSet resultSet = statement.getResultSet();
+            ResultSetMetaData metadata = resultSet.getMetaData();
+            int columnCount = metadata.getColumnCount();
+
+            while (resultSet.next()) {
+                StringBuffer row = new StringBuffer("<" + rowElement + ">");
+                for (int i = 1; i <= columnCount; i++) {
+                    String colName = metadata.getColumnLabel(i);
+                    int colType = metadata.getColumnType(i);
+                    String field = resultSet.getString(colName);
+                    row.append("<" + colName + ">");
+                    row.append(CommonUtil.escapeXml(field));
+                    row.append("</" + colName + ">");
+                }
+                row.append("</" + rowElement + ">");
+
+                queryResult.addVariable( new NodeVariable(row) );
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
+        
+        return queryResult;
     }
 
     public String[] getValidAttributes() {
-        return new String[] {"type", "connection"};
+        return new String[] {"jdbcclass", "connection", "username", "password"};
     }
 
     public String[] getRequiredAttributes() {
-        return new String[] {"connection"};
+        return new String[] {"jdbcclass", "connection"};
     }
 
     public String[] getValidSubprocessors() {
