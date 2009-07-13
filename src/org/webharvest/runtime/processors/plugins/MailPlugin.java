39d9a2f7a69f1be0f081d7b0ad38bd424941e9e7
vnikic <vnikic@451d0739-362e-0410-8668-fe84cbc230cf>
2009-07-13 21:07:37 +0000

    mail plugin continued...
    git-svn-id: https://web-harvest.svn.sourceforge.net/svnroot/web-harvest@199 451d0739-362e-0410-8668-fe84cbc230cf

--- src/org/webharvest/runtime/processors/plugins/MailPlugin.java
+++ src/org/webharvest/runtime/processors/plugins/MailPlugin.java
@@ -6,9 +6,7 @@ import org.webharvest.runtime.processors.*;
 import org.webharvest.runtime.variables.NodeVariable;
 import org.webharvest.runtime.variables.Variable;
 import org.webharvest.utils.CommonUtil;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
+import org.apache.commons.mail.*;
 
 /**
  * Sample plugin
@@ -20,17 +18,48 @@ public class MailPlugin extends WebHarvestPlugin {
     }
 
     public Variable executePlugin(Scraper scraper, ScraperContext context) {
-        Email email = new SimpleEmail();
+        Email email;
+
+        boolean isHtml = "html".equalsIgnoreCase(evaluateAttribute("type", scraper));
+        if (isHtml) {
+            email = new MultiPartEmail();
+        } else {
+            email = new SimpleEmail();
+        }
+
         email.setHostName( evaluateAttribute("smtp-host", scraper) );
         email.setSmtpPort( evaluateAttributeAsInteger("smtp-port", 25, scraper) );
+
         try {
             email.setFrom( evaluateAttribute("from", scraper) );
         } catch (EmailException e) {
             e.printStackTrace();
         }
+
+        for ( String replyTo:  CommonUtil.tokenize(evaluateAttribute("reply-to", scraper), ",") ) {
+            try {
+                email.addReplyTo(replyTo);
+            } catch (EmailException e) {
+                e.printStackTrace();
+            }
+        }
         for ( String to:  CommonUtil.tokenize(evaluateAttribute("to", scraper), ",") ) {
             try {
                 email.addTo(to);
+            } catch (EmailException e) {
+                e.printStackTrace();
+            }
+        }
+        for ( String cc:  CommonUtil.tokenize(evaluateAttribute("cc", scraper), ",") ) {
+            try {
+                email.addCc(cc);
+            } catch (EmailException e) {
+                e.printStackTrace();
+            }
+        }
+        for ( String bcc:  CommonUtil.tokenize(evaluateAttribute("bcc", scraper), ",") ) {
+            try {
+                email.addBcc(bcc);
             } catch (EmailException e) {
                 e.printStackTrace();
             }
@@ -51,10 +80,20 @@ public class MailPlugin extends WebHarvestPlugin {
             email.setSSL(true);
         }
 
-        try {
-            email.setMsg("This is a simple test of commons-email");
-        } catch (EmailException e) {
-            e.printStackTrace();
+        String charset = evaluateAttribute("charset", scraper);
+        if (CommonUtil.isEmptyString(charset)) {
+            charset = scraper.getConfiguration().getCharset();
+        }
+        email.setCharset(charset);
+
+        if (isHtml) {
+            BaseProcessor[] processors = getSubprocessors(scraper);
+        } else {
+            try {
+                email.setMsg(executeBody(scraper, context).toString());
+            } catch (EmailException e) {
+                e.printStackTrace();
+            }
         }
 
         try {
@@ -62,14 +101,26 @@ public class MailPlugin extends WebHarvestPlugin {
         } catch (EmailException e) {
             e.printStackTrace();
         }
-
-        BaseProcessor[] processors = getSubprocessors(scraper);
-
+        
         return new NodeVariable(getName());
     }
 
     public String[] getValidAttributes() {
-        return new String[] {"smtp-host", "smtp-port", "from", "reply-to", "to", "cc", "bcc", "subject", "charset", "username", "password", "security"};
+        return new String[] {
+                "smtp-host",
+                "smtp-port",
+                "type",
+                "from",
+                "reply-to",
+                "to",
+                "cc",
+                "bcc",
+                "subject",
+                "charset",
+                "username", 
+                "password",
+                "security"
+        };
     }
 
     public String[] getRequiredAttributes() {
